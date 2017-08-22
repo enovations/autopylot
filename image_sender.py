ac7ff5c96d6bbff79b4ac4088ce295de61913362
Klemen <klemen.pylo@gmail.com>
2017-08-22 09:54:15 +0200

    Fix picamera

--- image_sender.py
+++ image_sender.py
@@ -1,31 +1,46 @@
-import io
-import socket
-import struct
-import cv2
+# import io
+# import socket
+# import struct
+# import cv2
+#
+# client_socket = socket.socket()
+# client_socket.connect(('localhost', 8063))
+#
+# def send(image):
+#     try:
+#
+#         connection = client_socket.makefile('wb')
+#         stream = io.BytesIO()
+#
+#         # stream = cv2.imencode('.jpg', image)
+#
+#         # Write the length of the capture to the stream and flush to  # ensure it actually gets sent
+#         connection.write(struct.pack('<L', stream.tell()))
+#         connection.flush()
+#
+#         # Rewind the stream and send the image data over the wire
+#         stream.seek(0)
+#         connection.write(stream.read())
+#
+#         # Reset the stream for the next capture
+#         stream.seek(0)
+#         stream.truncate()
+#         # Write a length of zero to the stream to signal we're done
+#         connection.write(struct.pack('<L', 0))
+#     finally:
+#         connection.close()
+#         client_socket.close()
 
-client_socket = socket.socket()
-client_socket.connect(('localhost', 8063))
+import socket
+import pickle
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('localhost', 8089))
+
 
 def send(image):
-    try:
+    data = pickle.dumps(image)
 
-        connection = client_socket.makefile('wb')
-
-        stream = cv2.imencode('.jpg', image)
-
-        # Write the length of the capture to the stream and flush to  # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
-
-        # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
-
-        # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
-        # Write a length of zero to the stream to signal we're done
-        connection.write(struct.pack('<L', 0))
-    finally:
-        connection.close()
-        client_socket.close()
+    clientsocket.send("H" + len(data))
+    clientsocket.send("A")
+    clientsocket.send(data)
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# import the necessary packages
+import cv2
+import time
+
+import image_process
+import image_sender
+
 nopi = False
 
 try:
@@ -7,17 +12,13 @@ try:
 except:
     nopi = True
 
-import cv2
-import time
-
-import image_process
-
-# init camera
-camera = PiCamera()
-camera.resolution = (640, 480)
-#camera.shutter_speed = 6000000
-#camera.exposure_mode = 'off'
-#camera.iso = 800
+# init camera if can
+if not nopi:
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    # camera.shutter_speed = 6000000
+    # camera.exposure_mode = 'off'
+    # camera.iso = 800
 
 while True:
     if nopi:
@@ -27,5 +28,9 @@ while True:
         time.sleep(0.1)
         camera.capture(rawCapture, format='bgr')
         image = rawCapture.array
+
     image_process.transform_image(image)
+
+    image_sender.send(image)
+
     time.sleep(0.1)
