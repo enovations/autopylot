5edb10b5038e28a2551396a20df191e9df1a3947
Klemen <klemen.pylo@gmail.com>
2017-08-22 10:14:30 +0200

    Working video streamer

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cv2
 import time
 
+from flask import Flask, Response
+
 import image_process
-import image_sender
 
 nopi = False
 
@@ -20,7 +21,10 @@ if not nopi:
     # camera.exposure_mode = 'off'
     # camera.iso = 800
 
-while True:
+app = Flask(__name__)
+
+
+def rendered_image():
     if nopi:
         image = cv2.imread('sample.jpg')
     else:
@@ -29,8 +33,26 @@ while True:
         camera.capture(rawCapture, format='bgr')
         image = rawCapture.array
 
-    image_process.transform_image(image)
-
-    image_sender.send(image)
+        image = image_process.transform_image(image)
 
     time.sleep(0.1)
+
+    return image
+
+
+def gen():
+    while True:
+        image = rendered_image()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', image)[1].tostring() + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
