c3a0f7618b5ed3085683c00d272a8ab526e52dd1
Klemen <klemen.pylo@gmail.com>
2017-08-22 16:49:25 +0200

    Calibration

--- calibrate_transform.py
+++ calibrate_transform.py
@@ -1,7 +1,51 @@
 # this is a tool to calibrate transformation from sample.jpg
+import cv2
+import numpy as np
+
+clicks1 = []
+clicks = 0
+
+
+def click(event, x, y, flags, param):
+    global clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+        clicks += 1
+        if clicks < 5:
+            clicks1.append([x, y])
+        else:
+            print(clicks1)
+
+w, h = 640, 480
+
+# click on point in clockwise direction, first current, then desired shape
 
 image = cv2.imread('sample.jpg')
 
-cv2.imshow('image',img)
+cv2.resize(image, (w, h))
+
+m_rot = cv2.getRotationMatrix2D((w / 2, h / 2), 180, 1.0)
+image = cv2.warpAffine(image, m_rot, (w, h))
+
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click)
+
+cv2.imshow('image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+ow, oh = 205, 0
+wi = 180
+
+# test
+pts1 = np.float32(clicks1)
+# pts1 = np.float32([[153, 169], [427, 170], [486, 359], [61, 357]])
+
+matrix = cv2.getPerspectiveTransform(
+    pts1,
+    np.float32([[ow, oh+wi], [ow+wi, oh+wi], [ow+wi, oh], [ow, oh]])
+)
+image = cv2.warpPerspective(image, matrix, (w, h))
+
+cv2.imshow('image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- image_process.py
+++ image_process.py
@@ -2,13 +2,19 @@ import cv2
 import numpy as np
 
 
-def transform_image(image):
-    w, h = 1000, 750
-    img = cv2.resize(image, (w, h))
+def transform_image(img):
+
+    w, h = img.size
+
+    # rotate image
     m_rot = cv2.getRotationMatrix2D((w / 2, h / 2), 180, 1.0)
     img = cv2.warpAffine(img, m_rot, (w, h))
-    pts1 = np.float32([[178, 95], [522, 100], [32, 275], [511, 288]])
-    pts2 = np.float32([[177, 95], [401, 98], [178, 318], [398, 316]])
+
+    #transform image
+    ow, oh = 205, 0
+    wi = 180
+    pts1 = np.float32([[153, 169], [427, 170], [486, 359], [61, 357]])
+    pts2 = np.float32([[ow, oh+wi], [ow+wi, oh+wi], [ow+wi, oh], [ow, oh]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     img = cv2.warpPerspective(img, matrix, (w, h))
 
