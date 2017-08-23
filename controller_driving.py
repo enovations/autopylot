f190d215a0129200c0a3d91ec5f1a323edf89e6d
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-08-23 17:47:50 +0200

    fix

--- controller_driving.py
+++ controller_driving.py
@@ -6,8 +6,7 @@ def get_speed(r, filter):
     if r == 0:
         speed = max_speed
     else:
-        speed = max_speed * r * 0.15
+        speed = max_speed * abs(r) * 0.15
 
     v = filter.get([min(speed, max_speed)])
-    print(v)
     return v
