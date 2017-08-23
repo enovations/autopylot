dd5fef7145968c5e7bcdefe100e4efeba3ce7a7b
Vid Drobnic <vid.drobnic@gmail.com>
2017-08-23 10:35:11 +0200

    ros working. probably.

--- ros_control.py
+++ ros_control.py
@@ -6,12 +6,23 @@ try:
 except:
     noros = True
 
+robot_pub = None
+
 
 def init():
+    global robot_pub
     if not noros:
         robot_pub = rospy.Publisher('/robot/cmd_vel', Twist, queue_size=0)
         pntlt_pub = rospy.Publisher('/pan_tilt/cmd_vel', Twist, queue_size=0)
 
 
-def update_robot(v, w):
-    pass
+def update_robot(v, r):
+    global robot_pub
+
+    if r == 0:
+        w = 0
+    else:
+        w = v / r
+
+    message = Twist(Vector3(float(v), 0, 0), Vector3(0, 0, float(w)))
+    robot_pub.publish(message)
