3106af373c7b5ec7782b2cb04e3f6231bf482813
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-08-23 17:31:02 +0200

    redesign filter

--- filter.py
+++ filter.py
@@ -3,9 +3,10 @@ import __conf__
 
 
 class Filter:
-    def __init__(self, queue_size, weights):
-        self.q = deque([[0 for _ in range(len(weights))] for _ in range(queue_size)])
+    def __init__(self, weights, num_of_fields):
+        self.q = deque([[0 for _ in range(num_of_fields)] for _ in range(len(weights))])
         self.weights = weights
+        self.num_of_fields = num_of_fields
 
     def get(self, new_values):
         self.q.popleft()
@@ -13,7 +14,12 @@ class Filter:
         return self.calculate()
 
     def calculate(self):
-        return [sum(elements[i] * self.weights[j] for j, elements in enumerate(self.q)) / sum(self.weights) / sum(self.weights) for i in range(len(self.weights))]
+        result = []
+        for i in range(self.num_of_fields):
+            vsota = sum([element[i]*self.weights[j] for j, element in enumerate(self.q)])
+            result.append(vsota / sum(self.weights))
+        return result
+        # return [sum(elements[i] * self.weights[j] for j, elements in enumerate(self.q)) / sum(self.weights) / sum(self.weights) for i in range(len(self.weights))]
         # return sum(tupl[0]*self.weights[i] for i, tupl in enumerate(self.q)) / sum(self.weights), sum(tupl[2]*__conf__.position_gain*self.weights[i] for i, tupl in enumerate(self.q)) / sum(self.weights)
 
     @staticmethod
