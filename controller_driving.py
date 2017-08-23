5dde4dcde79c37533e9c885522344ff01cbb7997
Vid Drobnic <vid.drobnic@gmail.com>
2017-08-23 17:28:37 +0200

    bessar controller_driving

--- controller_driving.py
+++ controller_driving.py
@@ -1,11 +1,11 @@
 import controller_traffic
 
 
-def get_speed(w):
+def get_speed(r):
     max_speed = controller_traffic.get_speed_limit()
-    if w == 0:
+    if r == 0:
         speed = max_speed
     else:
-        speed = max_speed * (1.0 / w)
+        speed = max_speed * r * 0.15
     # TODO: Filter when ziga is done with filter
-    return speed
+    return min(speed, max_speed)
