882a7792769e1c279b256455434c2f87913cf491
Klemen <klemen.pylo@gmail.com>
2017-08-23 14:01:10 +0200

    Better conf, calibration

--- __conf__.py
+++ __conf__.py
@@ -13,7 +13,7 @@ v = 0.05  # speed of robot in m/s
 # coordinate system calibration
 pixel_25cm_distance = 40
 first_cut_to_image_edge_in_pixels = 17
-meter_to_pixel_ratio = 0.25 / pixel_25cm_distance # ration of pixels on screen to meters in real world
+meter_to_pixel_ratio = 0.25 / pixel_25cm_distance  # ratio of pixels on screen to meters in real world
 angularvel_factor_p = 50
-tx, ty, wi = 248, 117, 265 # transformation result offset x, offset y and length of square
-orig_sqre = [[219, 146], [602, 145], [528, 402], [244, 403]] # original map of pixels: define in clockwise direction
+tx, ty, wi = 248, 117, 265  # transformation result offset x, offset y and length of square
+orig_sqre = [[219, 146], [602, 145], [528, 402], [244, 403]]  # original map of pixels: define in clockwise direction
