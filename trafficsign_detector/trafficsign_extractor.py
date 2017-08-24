4447ab284c40b598fc89d28cc4c6d95883a480ff
Klemen <klemen.pylo@gmail.com>
2017-08-24 12:10:53 +0200

    Sample signs

--- trafficsign_detector/trafficsign_extractor.py
+++ trafficsign_detector/trafficsign_extractor.py
@@ -3,24 +3,28 @@ import numpy as np
 
 import image_process
 
-image = cv2.imread('known_signs/3.jpg')
+for i2 in range(1, 13):
 
-image = image_process.transform_image(image)
+    image = cv2.imread('known_signs/'+str(i2)+'.jpg')
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-edged = cv2.Canny(gray, 30, 200)
+    image = image_process.transform_image(image)
 
-im2, contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    edged = cv2.Canny(gray, 30, 200)
 
-for con in contours:
-    rect = cv2.minAreaRect(con)
-    box = cv2.boxPoints(rect)
-    box = np.float32(box)
+    im2, contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:20]
 
-    matrix = cv2.getPerspectiveTransform(box, np.float32([[0, 0], [0, 64], [64, 64], [64, 0]]))
-    img = cv2.warpPerspective(image, matrix, (64, 64))
+    i = 0
+    for con in contours:
+        rect = cv2.minAreaRect(con)
+        box = cv2.boxPoints(rect)
+        box = np.float32(box)
 
-    cv2.imshow("img", img)
-    cv2.waitKey(0)
+        matrix = cv2.getPerspectiveTransform(box, np.float32([[0, 0], [0, 48], [48, 48], [48, 0]]))
+        img = cv2.warpPerspective(image, matrix, (48, 48))
+
+        cv2.imwrite('known_signs/'+str(i2)+'_' + str(i) + '.jpg', img)
+
+        i += 1
