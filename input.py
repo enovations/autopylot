e946355c2f9e5c5f782eaee0e359c08f1ba06db3
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-08-25 09:27:11 +0200

    add input

--- input.py
+++ input.py
@@ -1,1 +1,14 @@
-asdf
+import socket
+import atexit
+
+client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+client.connect('/tmp/autopylot_input_socket')
+
+while True:
+    dest = input('select dest: ')
+    client.send(dest.encode('utf8'))
+
+
+@atexit.register
+def stop():
+    client.close()
